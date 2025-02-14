761028bf736d517ff78c7c3aed962ced895cd33e
Vikas Nigam <vikasnigam531942@gmail.com>
2025-02-14 12:56:12 +0530

    added multiply

---
+++ src/main/java/firstPackage/Multiply.java
@@ -0,0 +1,13 @@
+package firstPackage;
+
+public class Multiply {
+	public static void main(String[] args) {
+		int a,b,c;
+		a=10;
+		b=10;
+		c=a*b;
+		System.out.println(" sum is " +c);
+	}
+
+
+}
